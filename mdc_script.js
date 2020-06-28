e405d03dfbf0a1c9bc0617047da9586e7eca567b
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2020-06-28 16:17:07 +0900

    test 3

--- mdc_script.js
+++ mdc_script.js
@@ -1,4 +1,4 @@
-const {MDCSlider} = require('@material/slider/dist/mdc.slider');
+const {MDCSlider} = require('./node_modules/@material/slider/dist/mdc.slider');
 
 const slider = new MDCSlider(document.querySelector('.mdc-slider'));
 slider.lister('MDCSlider:change', () => console.log(`Value changed to ${slider.value}`));
