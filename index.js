36cbf6fa56a0648314103d58bd5c42b87117b65c
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2020-06-29 13:42:18 +0900

    test 7

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 const {MDCSlider} = require('@material/slider');
 
-const change_col_blur = (color, val) => {
-    val = val.toFixed(1);
+const change_col_blur_offset = (color, blur, x, y) => {
+    blur = blur.toFixed(1);
+    x = x.toFixed(1);
+    y = y.toFixed(1);
     color = color.replace(/\#/, '');
     let rgb = [];
     for(let i = 0; i < 6; i += 2){
         rgb.push(color[i] + color[i + 1]);
     }
+    let contrast_flag = false;
     for(let i of rgb){
-        if(parseInt(i, 16) < (255 / 2)){
-            color_div.setAttribute('style', 'color: #ffffff');
-            blur_div.setAttribute('style', 'color: #ffffff');
-            mdc_slider.setAttribute('style', '--mdc-theme-secondary: #ffffff;');
-            break;
+        if(parseInt(i, 16) > 255 - 255 / 3) contrast_flag = true;
+    }
+    if(!contrast_flag){
+        for(let d of wrapper_div){
+            d.setAttribute('style', 'color: #ffffff');
         }
-        if(parseInt(i, 16) > (255 / 2)){
-            color_div.setAttribute('style', 'color: rgba(0, 0, 0, 0.87)');
-            blur_div.setAttribute('style', 'color: rgba(0, 0, 0, 0.87)');
-            mdc_slider.setAttribute('style', '--mdc-theme-secondary: #000000;');
+        color_input_hex.setAttribute('style', 'border-bottom: 1px solid #ffffff');
+        for(let s of sizing_input){
+            s.setAttribute('style', 'border-bottom: 1px solid #ffffff');
+        }
+        lighting_select.setAttribute('style', '--mdc-theme-primary: #ffffff')
+        lighting_select_icon.setAttribute('style', 'fill: #ffffff')
+        lighting_selected_text.setAttribute('style', 'color: #ffffff');
+        lighting_ripple_before.innerHTML = '#controller #light .mdc-line-ripple::before{border-bottom-color: #ffffff;}';
+        for(let s of mdc_slider){
+            s.setAttribute('style', '--mdc-theme-secondary: #ffffff;');
+        }
+    }else{
+        for(let d of wrapper_div){
+            d.setAttribute('style', 'color: rgba(0, 0, 0, 0.87)');
+        }
+        color_input_hex.setAttribute('style', 'border-bottom: 1px solid rgba(0, 0, 0, 0.87)');
+        for(let s of sizing_input){
+            s.setAttribute('style', 'border-bottom: 1px solid rgba(0, 0, 0, 0.87)');
+        }
+        lighting_select_icon.setAttribute('style', 'fill: rgba(0, 0, 0, 0.87);')
+        lighting_select.setAttribute('style', '--mdc-theme-primary: rgba(0, 0, 0, 0.87);')
+        lighting_selected_text.setAttribute('style', 'color: rgba(0, 0, 0, 0.87);');
+        lighting_ripple_before.innerHTML = '#controller #light .mdc-line-ripple::before{border-bottom-color: rgba(0, 0, 0, 0.87);}';
+        for(let s of mdc_slider){
+            s.setAttribute('style', '--mdc-theme-secondary: rgba(0, 0, 0, 0.87);');
         }
     }
     let shadow = [];
@@ -35,31 +59,118 @@ const change_col_blur = (color, val) => {
     color = '#' + color;
     shadow = '#' + shadow.join('');
     color_input_map.value = color;
-    color_div_map.setAttribute('style', `background-color: ${color}; filter: drop-shadow(0px 0px ${val}px ${shadow})`);
+    color_div_map.setAttribute('style', `background-color: ${color}; filter: drop-shadow(${x}px ${y}px ${blur}px ${shadow})`);
     color_input_hex.value = color;
+    lighting_anchor.innerHTML = `#controller #light .mdc-select__anchor{background-color: ${color};}`;
     code_color_bg.innerText = color;
     code_color_ds.innerText = shadow;
-    code_blur.innerText = `${val}px`;
+    code_blur.innerText = `${blur}px`;
+    code_offset_x.innerText = `${x}px`;
+    code_offset_y.innerText = `${y}px`;
     container.setAttribute('style', `background-color: ${color}`);
-    target.setAttribute('style', `background-color: ${color}; filter: drop-shadow(0px 0px ${val}px ${shadow})`);
-    controller.setAttribute('style', `background-color: ${color}; filter: drop-shadow(0px 0px ${val}px ${shadow})`);
+    target.setAttribute('style', `background-color: ${color}; filter: drop-shadow(${x}px ${y}px ${blur}px ${shadow})`);
+    controller.setAttribute('style', `background-color: ${color}; filter: drop-shadow(${x}px ${y}px ${blur}px ${shadow})`);
+    sizing();
+    return shadow;
 };
 
-const slider = new MDCSlider(document.querySelector('.mdc-slider'));
-slider.listen('MDCSlider:input', () => {
-    console.log('hoge');
-    change_col_blur(color_input_hex.value, slider.value);
+const sizing = () => {
+    let x = sizing_x.value; let y = sizing_y.value;
+    let tmp = target.getAttribute('style');
+    target.setAttribute('style', `${tmp}; height: ${y}px; width: ${x}px;`);
+    code_height.innerText = `${y}px`;
+    code_width.innerText = `${x}px`;
+};
+
+const blur_slider = new MDCSlider(document.querySelector('#blur_slider'));
+const offset_x_slider = new MDCSlider(document.querySelector('#offset_x_slider'));
+const offset_y_slider = new MDCSlider(document.querySelector('#offset_y_slider'));
+const lighting_scale_slider = new MDCSlider(document.querySelector('#lighting_scale_slider'))
+
+blur_slider.listen('MDCSlider:input', () => {
+    change_col_blur_offset(color_input_hex.value, blur_slider.value, offset_x_slider.value * lighting_scale_slider.value.toFixed(1), offset_y_slider.value * lighting_scale_slider.value.toFixed(1));
+});
+
+offset_x_slider.listen('MDCSlider:input', () => {
+    change_col_blur_offset(color_input_hex.value, blur_slider.value, offset_x_slider.value * lighting_scale_slider.value.toFixed(1), offset_y_slider.value * lighting_scale_slider.value.toFixed(1));
+});
+
+offset_y_slider.listen('MDCSlider:input', () => {
+    change_col_blur_offset(color_input_hex.value, blur_slider.value, offset_x_slider.value * lighting_scale_slider.value.toFixed(1), offset_y_slider.value * lighting_scale_slider.value.toFixed(1));
+});
+
+lighting_scale_slider.listen('MDCSlider:input', () => {
+    console.log(lighting_scale_slider.value);
+    let y = offset_y_slider.value * lighting_scale_slider.value.toFixed(1);
+    let x = offset_x_slider.value * lighting_scale_slider.value.toFixed(1);
+    change_col_blur_offset(color_input_hex.value, blur_slider.value, x, y);
 });
 
 color_input_map.addEventListener('input', () => {
-    change_col_blur(color_input_map.value, slider.value);
+    change_col_blur_offset(color_input_map.value, blur_slider.value, offset_x_slider.value * lighting_scale_slider.value.toFixed(1), offset_y_slider.value * lighting_scale_slider.value.toFixed(1));
 });
 
-color_input_hex.addEventListener('change', () => {
-    change_col_blur(color_input_hex.value, slider.value);
+color_input_hex.addEventListener('input', () => {
+    change_col_blur_offset(color_input_hex.value, blur_slider.value, offset_x_slider.value * lighting_scale_slider.value.toFixed(1), offset_y_slider.value * lighting_scale_slider.value.toFixed(1));
+});
+
+sizing_x.addEventListener('input', () => {
+    sizing();
+});
+
+sizing_y.addEventListener('input', () => {
+    sizing();
+});
+
+const {MDCSelect} = require('@material/select');
+const lighting_select_handler = new MDCSelect(document.querySelector('#lighting_mdc_select'));
+lighting_select_handler.listen('MDCSelect:change', () => {
+    // alert(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
+    let x, y;
+    switch(lighting_select_handler.value){
+        case 'top left':
+            x = 10; y = 10;
+            break;
+        case 'top center':
+            x = 0; y = 10;
+            break;
+        case 'top right':
+            x = -10; y = 10;
+            break;
+        case 'center left':
+            x = 10; y = 0;
+            break;  
+        case 'center':
+            x = 0; y = 0;
+            break;
+        case 'center right':
+            x = -10; y = 0;
+            break;
+        case 'bottom left':
+            x = 10; y = -10;
+            break;
+        case 'bottom center':
+            x = 0; y = -10;
+            break;
+        case 'bottom right':
+            x = -10; y = -10;
+            break;
+    }
+    lighting_scale_slider.value = 1;
+    offset_x_slider.value = x; offset_y_slider.value = y;
+    change_col_blur_offset(color_input_hex.value, blur_slider.value, x, y);
+});
+
+copy.addEventListener('click', () => {
+    let tmp = document.createElement('textarea');
+    tmp.innerHTML = code.innerText.replace(/content_copy/, '').replace(/\n/g, '').replace(/;/g, ';\n');
+    document.body.appendChild(tmp);
+    tmp.select();
+    document.execCommand('copy');
+    document.body.removeChild(tmp);
 });
 
 const init = () => {
-    change_col_blur('627fd5', slider.value);
+    change_col_blur_offset("627fd5", blur_slider.value, offset_x_slider.value * lighting_scale_slider.value.toFixed(1), offset_y_slider.value * lighting_scale_slider.value.toFixed(1));
 };
 init();
--- variables.js
+++ variables.js
@@ -1,13 +1,33 @@
-const color_div = document.querySelector('#color');
+const wrapper_div = document.querySelectorAll('.wrapper_div');
+
 const color_input_map = document.querySelector('#color_input_map');
 const color_div_map = document.querySelector('#color_div_map');
 const color_input_hex = document.querySelector('#color_input_hex');
-const blur_div = document.querySelector('#blur');
-const mdc_slider = document.querySelector('.mdc-slider');
+
+const sizing_input = document.querySelectorAll('.sizing_input');
+const sizing_x = document.querySelector('#x_input');
+const sizing_y = document.querySelector('#y_input');
+
+const mdc_slider = document.querySelectorAll('.mdc-slider');
+
+const lighting_select = document.querySelector('.mdc-select');
+const lighting_select_icon = document.querySelector('.mdc-select__dropdown-icon');
+let lighting_ripple_before = document.createElement('style');
+document.body.appendChild(lighting_ripple_before);
+let lighting_anchor = document.createElement('style');
+document.body.appendChild(lighting_anchor);
+const lighting_selected_text = document.querySelector('#selected-text');
+
 const code = document.querySelector('#code');
+const copy = document.querySelector('#copy');
+const code_height = document.querySelector('#code_height');
+const code_width = document.querySelector('#code_width');
 const code_color_bg = document.querySelector('#code_color_bg');
 const code_color_ds = document.querySelector('#code_color_ds');
 const code_blur = document.querySelector('#code_blur');
+const code_offset_x = document.querySelector('#code_offset_x');
+const code_offset_y = document.querySelector('#code_offset_y');
+
 const container = document.querySelector('#container');
 const target = document.querySelector('#target');
 const controller = document.querySelector('#controller');
